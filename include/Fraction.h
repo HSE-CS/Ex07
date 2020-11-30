626da8c70c07cdf8719549fae17adc4c5ecbf8fb
stifeevn <stifeev.nikita@mail.ru>
2020-11-30 23:38:39 +0500

    ex07

---
+++ include/Fraction.h
@@ -0,0 +1,26 @@
+// Copyright 2020 Nikita Stifeev
+#ifndef INCLUDE_FRACTION_H_
+#define INCLUDE_FRACTION_H_
+
+#include <string>
+#include <stdexcept>
+
+class Fraction {
+ private:
+    int numerator;
+    int denominator;
+    int gcd(int number1, int number2) const;
+    void normalize();
+ public:
+    explicit Fraction(int n = 0, int m = 1);
+    Fraction(const Fraction& fraqtion);
+    Fraction& operator=(const Fraction& fraqtion);
+    int getNumerator() const;
+    int getDenominator() const;
+    Fraction operator+(const Fraction& fraqtion) const;
+    Fraction operator-(const Fraction& fraqtion) const;
+    Fraction operator*(const Fraction& fraqtion) const;
+    Fraction operator/(const Fraction& fraqtion) const;
+    std::string getValue() const;
+};
+#endif  // INCLUDE_FRACTION_H_
