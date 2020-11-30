1df9f8e5f1fadd9aa7b3bf6c9c773b613a6a3fe8
Eugene <evd.jeka@gmail.com>
2020-11-30 16:37:26 +0300

    make cpplint happy

--- include/Fraction.h
+++ include/Fraction.h
@@ -9,13 +9,13 @@ class Fraction {
   void normalize();
 
  public:
-  Fraction(int32_t numer = 0, int32_t denom = 1) : numer(numer), denom(denom) {
+  explicit Fraction(int32_t numer = 0, int32_t denom = 1) : numer(numer), denom(denom) {
     if (denom == 0) {
       throw "Divide by zero";
     }
     normalize();
-  };
-  Fraction(Fraction& other) : numer(other.numer), denom(other.denom){};
+  }
+  Fraction(Fraction& other) : numer(other.numer), denom(other.denom){}
   std::string getValue();
   int32_t getNumerator();
   int32_t getDenominator();
