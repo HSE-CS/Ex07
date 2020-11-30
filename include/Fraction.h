3bc73fc5f880d469c6c42e5ed13a689c62d830f3
EgorBuzanov <egor4ek007@gmail.com>
2020-11-30 22:29:42 +0300

    Egor Buzanov

---
+++ include/Fraction.h
@@ -0,0 +1,28 @@
+// Copyright 2020 Egor Buzanov
+#ifndef EX07_INCLUDE_FRACTION_H_
+#define EX07_INCLUDE_FRACTION_H_
+
+#include <cmath>
+#include <iostream>
+#include <numeric>
+#include <string>
+
+class Fraction {
+ private:
+  int numerator, denominator;
+  void normalize();
+
+ public:
+  explicit Fraction(int num = 0, int den = 1);
+  Fraction(const Fraction &);
+  std::string getValue();
+  int getNumerator();
+  int getDenominator();
+  Fraction operator+(const Fraction &);
+  Fraction operator-(const Fraction &);
+  Fraction operator*(const Fraction &);
+  Fraction operator/(const Fraction &);
+  Fraction operator=(const Fraction &);
+};
+
+#endif  // EX07_INCLUDE_FRACTION_H_
---
+++ src/Fraction.cpp
@@ -0,0 +1,70 @@
+// Copyright 2020 Egor Buzanov
+#include "../include/Fraction.h"
+
+void Fraction::normalize() {
+  if (numerator < 0 && denominator < 0) {
+    numerator = -numerator;
+    denominator = denominator;
+  }
+
+  int gcd = std::gcd(numerator, denominator);
+  numerator /= gcd;
+  denominator /= gcd;
+};
+
+explicit Fraction::Fraction(int num = 0, int den = 1) {
+  if (den != 0) {
+    numerator = num;
+    denominator = den;
+    this->normalize();
+  } else {
+    throw "Division by 0";
+  }
+};
+
+Fraction::Fraction(const Fraction &copied_frac) {
+  numerator = copied_frac.numerator;
+  denominator = copied_frac.denominator;
+};
+
+std::string Fraction::getValue() {
+  std::string result;
+  if (numerator < 0 || denominator < 0) {
+    result += '-';
+  }
+
+  result += std::to_string(abs(numerator));
+
+  if (denominator != -1 && denominator != 1) {
+    result += '/' + std::to_string(abs(denominator));
+  }
+
+  return result;
+};
+
+int Fraction::getNumerator() { return numerator; };
+
+int Fraction::getDenominator() { return denominator; };
+
+Fraction Fraction::operator+(const Fraction &frac) {
+  return Fraction(numerator * frac.denominator + frac.numerator * denominator,
+                  denominator * frac.denominator);
+};
+
+Fraction Fraction::operator-(const Fraction &frac) {
+  return Fraction(numerator * frac.denominator - frac.numerator * denominator,
+                  denominator * frac.denominator);
+};
+
+Fraction Fraction::operator*(const Fraction &frac) {
+  return Fraction(numerator * frac.numerator, denominator * frac.denominator);
+};
+
+Fraction Fraction::operator/(const Fraction &frac) {
+  return Fraction(numerator * frac.denominator, denominator * frac.numerator);
+};
+
+Fraction Fraction::operator=(const Fraction &frac) {
+  numerator = frac.numerator;
+  denominator = frac.denominator;
+};
