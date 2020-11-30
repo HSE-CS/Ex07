39ab043cac6ec09366f27c505318c96855d3404e
NikDemoShow <Nixon2001@yandex.ru>
2020-11-30 19:32:04 +0300

    let's check that's victory

--- include/Fraction.h
+++ include/Fraction.h
@@ -8,14 +8,14 @@
 class Fraction {
  private:
 int numerator, denominator;
-   void normalize();
+  void normalize();
  public:
-   Fraction();
-   explicit Fraction(int new_numerator, int new_denominator);
-   Fraction(const Fraction& F);
-   std::string getValue() const;
-   int getNumerator() const;
-   int getDenominator() const;
+  Fraction();
+  explicit Fraction(int new_numerator, int new_denominator);
+  Fraction(const Fraction& F);
+  std::string getValue() const;
+  int getNumerator() const;
+  int getDenominator() const;
 };
 int NOD(int a, int b);
 Fraction operator+(const Fraction& a, const Fraction& b);
