f3b0f51ef8afe0db75027ffb776a41d156dd2180
NikDemoShow <Nixon2001@yandex.ru>
2020-11-30 19:16:13 +0300

    One more time

--- include/Fraction.h
+++ include/Fraction.h
@@ -1,26 +1,25 @@
-//Copyright 2020 <Copyright NikDemoShow>
+// Copyright 2020 <Copyright NikDemoShow>
 #ifndef INCLUDE_FRACTION_H_
 #define INCLUDE_FRACTION_H_
 #include<sstream>
 #include<iostream>
+#include<string>
 
 class Fraction {
  private:
-	int numerator, denominator;
-	void normalize(); //закрытый метод нормировки
+   int numerator, denominator;
+   void normalize();
  public:
-	explicit Fraction();
-	explicit Fraction(int new_numerator, int new_denominator);
-	Fraction(const Fraction& F);
-	std::string getValue() const;
-	int getNumerator() const;
-	int getDenominator() const;
+   Fraction();
+   explicit Fraction(int new_numerator, int new_denominator);
+   Fraction(const Fraction& F);
+   std::string getValue() const;
+   int getNumerator() const;
+   int getDenominator() const;
 };
-
 int NOD(int a, int b);
 Fraction operator+(const Fraction& a, const Fraction& b);
 Fraction operator-(const Fraction& a, const Fraction& b);
 Fraction operator*(const Fraction& a, const Fraction& b);
 Fraction operator/(const Fraction& a, const Fraction& b);
-
 #endif  // INCLUDE_FRACTION_H_
