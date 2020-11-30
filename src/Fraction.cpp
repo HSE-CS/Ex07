aa14fba8a604bded383882fab043566dbe6244aa
natasha <igum.natasha@gmail.com>
2020-12-01 00:52:01 +0300

    Initial commit

---
+++ src/Fraction.cpp
@@ -0,0 +1,73 @@
+//Copyright 2020 Igumnova Natasha
+#include "Fraction.h"
+
+int NOD(int a, int b)
+{
+	while (a != 0 && b != 0)
+	{
+		if (a > b)
+			a %= b;
+		else
+			b %= a;
+	}
+	return a + b;
+}
+Fraction Fraction::normalize()
+{
+	int nod = NOD(numerator, denominator);
+	numerator /= nod;
+	denominator /= nod;
+	return *this;
+}
+
+std::string Fraction::getValue()
+{
+	Fraction f = normalize();
+	char char_num = f.numerator + '0';
+	char char_den = f.denominator + '0';
+	std::string(res);
+	res.append(1, char_num);
+	if (f.denominator != 1)
+	{
+		res.append(1, '\\');
+		res.append(1, char_den);
+	}
+	return res;
+}
+
+int Fraction::getNumerator() const
+{
+	return numerator;
+}
+
+int Fraction::getDenominator() const
+{
+	return denominator;
+}
+
+Fraction Fraction::operator+(const Fraction& f)
+{
+	return Fraction(numerator * f.denominator + f.numerator * denominator, denominator * f.denominator).normalize();
+}
+
+Fraction Fraction::operator-(const Fraction& f)
+{
+	return Fraction(numerator * f.denominator - f.numerator * denominator, denominator * f.denominator).normalize();
+}
+
+Fraction Fraction::operator*(const Fraction& f)
+{
+	return Fraction(numerator * f.numerator, denominator * f.denominator).normalize();
+}
+
+Fraction Fraction::operator/(const Fraction& f)
+{
+	return Fraction(numerator * f.denominator, f.numerator * denominator).normalize();
+}
+
+Fraction Fraction::operator=(const Fraction& f)
+{
+	numerator = f.numerator;
+	denominator = f.denominator;
+	return *this;
+}
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+//Copyright 2020 Igumnova Natasha
+#include "Fraction.h"
+
+int main()
+{
+	Fraction z(1, 2); // 1/2
+	Fraction x(z);
+	Fraction y;
+	std::cout << y.getValue() << std::endl; // 0
+	y = z + x;
+	std::cout << y.getValue() << std::endl; // 1
+	Fraction a(1, 3);
+	Fraction b(2, 3);
+	Fraction c;
+	c = a + b;
+	std::cout << c.getNumerator() << std::endl;
+	std::cout << c.getDenominator() << std::endl;
+	std::cout << c.getValue() << std::endl;
+}
