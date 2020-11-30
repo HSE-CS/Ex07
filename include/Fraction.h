e996bb28b6c638592e6968d74eeafb9bf6190ff3
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-30 18:03:36 +0300

    created Fraction template

--- include/Fraction.h
+++ include/Fraction.h
@@ -5,4 +5,41 @@
 #ifndef GHA_FRAC_FRACTION_H
 #define GHA_FRAC_FRACTION_H
 
+#include <cstdint>
+#include <string>
+
+class Fraction {
+private:
+
+    int32_t numerator, denominator;
+
+    void normalize();
+
+public:
+
+    explicit Fraction(int32_t numerator = 0, int32_t denominator = 1) : numerator(numerator), denominator(denominator) {
+        if (denominator == 0) throw "Exception in thread \"main\" ArithmeticException: Divide by zero";
+        normalize();
+    }
+
+    Fraction(Fraction &fraction) : numerator(fraction.numerator), denominator(fraction.denominator) {}
+
+    std::string getValue() const;
+
+    int32_t getNumerator() const;
+
+    int32_t getDenominator() const;
+
+    Fraction operator+(const Fraction &fraction) const;
+
+    Fraction operator-(const Fraction &fraction) const;
+
+    Fraction operator*(const Fraction &fraction) const;
+
+    Fraction operator/(const Fraction &fraction) const;
+
+    Fraction &operator=(const Fraction &fraction);
+
+}
+
 #endif
