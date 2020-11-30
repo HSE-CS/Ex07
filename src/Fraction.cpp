442baca2d399cb70b3a2d3fb5097a873cc4205e0
Vladislav-Kanev <vokanev@edu.hse.ru>
2020-12-01 00:44:10 +0300

    i hate stupid rule about 80 symbols and long names of variables

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -6,7 +6,11 @@ void Fraction::normalize() {
         this->denominator = 1;
         return;
     } else {
-        if ((numerator < 0 && denominator < 0) || (numerator > 0 && denominator < 0)) {
+        if (numerator < 0 && denominator < 0) {
+            numerator *= -1;
+            denominator *= -1;
+        }
+        else if (numerator > 0 && denominator < 0) {
             numerator *= -1;
             denominator *= -1;
         }
