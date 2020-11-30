1dfb24093a927960457c22d958afcd98a3e146d4
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2020-11-30 18:13:04 +0300

    add debug show

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,16 @@
  * @author Stanislav Stoianov
  */
 
+#include <iostream>
+#include "../include/Fraction.h"
+
+
+int main() {
+    Fraction fraction1(4, 7);
+    Fraction fraction2(fraction1);
+    Fraction fraction3;
+    std::cout << fraction3.getNumerator() << " / " << fraction3.getDenominator() << std::endl;
+    fraction3 = fraction1 + fraction2;
+    std::cout << fraction3.getValue() << std::endl;
+    return 0;
+}
