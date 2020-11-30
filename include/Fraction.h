570c8581ac7392004c25495704b807a8bfdb60a6
KuznetsovMikhail <KuznetsovMikhail>
2020-11-30 15:35:10 +0300

    initial

---
+++ include/Fraction.h
@@ -0,0 +1,4 @@
+// Copyright 2020 Kuznetsov Mikhail
+#ifndef FRACTION_H_INCLUDED
+#define FRACTION_H_INCLUDED
+#endif // FRACTION_H_INCLUDED
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+// Copyright 2020 Kuznetsov Mikhail
+int main()
+{
+    return 0;
+}
