0d1f51e3964243755241dafc3db3aeaf1f189c5d
gfgfgfgf1 <71260710+gfgfgfgf1@users.noreply.github.com>
2020-12-01 00:27:29 +0300

    ok, maybe thats right

--- include/Fraction.h
+++ include/Fraction.h
@@ -7,21 +7,21 @@
 #include <sstream>
 
 class Fraction {
-    private:
-        int numerator;
-        int denominator;
-        void normalize();
-    public:
-        Fraction();
-        explicit Fraction(int new_numerator, int new_denominator);
-        Fraction(const Fraction& fraction);
-        std::string getValue();
-        int getNumerator() const;
-        int getDenominator() const;
-        Fraction operator+(const Fraction& fc);
-        Fraction operator-(const Fraction& fc);
-        Fraction operator*(const Fraction& fc);
-        Fraction operator/(const Fraction& fc);
-        Fraction operator=(const Fraction& fc);
+ private:
+    int numerator;
+    int denominator;
+    void normalize();
+ public:
+    Fraction();
+    explicit Fraction(int new_numerator, int new_denominator);
+    Fraction(const Fraction& fraction);
+    std::string getValue();
+    int getNumerator() const;
+    int getDenominator() const;
+    Fraction operator+(const Fraction& fc);
+    Fraction operator-(const Fraction& fc);
+    Fraction operator*(const Fraction& fc);
+    Fraction operator/(const Fraction& fc);
+    Fraction operator=(const Fraction& fc);
 };
 #endif  // INCLUDE_FRACTION_H_
