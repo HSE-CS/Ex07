73aa9a9bbb321a8e682ce831b347187cdfdb8d5c
Kristina Egorova <kristinaeo2001@mail.ru>
2020-11-30 22:46:17 +0300

    First try

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright 2020 GHA Test Team
+#include <iostream>
+#include "Fraction.h"
+
+int main(){
+   Fraction a(1, 2), b(2, 3), c;
+   c = a/b;
+   std::cout << c.getValue();
+   return 0;
+}
