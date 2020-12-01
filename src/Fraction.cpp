348978018566632765e0bb440f2e792d6f76ec87
AlexShi23 <alieksandr_shirokov@list.ru>
2020-12-01 23:44:53 +0300

    style fix 4 (please last)

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -24,7 +24,8 @@ Fraction::Fraction(const Fraction& f) {
 int NOD(int a, int b) {
   while (a > 0 && b > 0) {
   if (a > b) a = a % b;
-  else b = b % a;
+  else 
+  b = b % a;
   }
   return a + b;
 }
