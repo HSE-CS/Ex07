0e72a559958bac6498ef88534a2b4dd654711a61
Karina Mashanova <karmen.01@inbox.ru>
2020-12-01 13:10:54 +0300

    first

--- include/Fraction.h
+++ include/Fraction.h
@@ -6,23 +6,26 @@
 #include <numeric>
 #include <algorithm>
 
-	class Fraction{
-		private:
-			int numerator;
-			int denominator;
-			void normalize();
-		public:
-			explicit Fraction(int num = 0, int denom = 1);
-			Fraction(const Fraction& fr);
-			std::string getValue();
-			int getNumerator();
-			int getDenominator();
-			Fraction operator+(const Fraction& fr);
-			Fraction operator-(const Fraction& fr);
-			Fraction operator*(const Fraction& fr);
-			Fraction operator/(const Fraction& fr);
-			Fraction operator=(const Fraction& fr); 
+	
+class Fraction{
+	private:
+		int numerator;
+		int denominator;
+		void normalize();
+	public:
+		explicit Fraction(int num = 0, int denom = 1);
+		Fraction(const Fraction& fr);
+		std::string getValue();
+		int getNumerator();
+		int getDenominator();
+		Fraction operator+(const Fraction& fr);
+		Fraction operator-(const Fraction& fr);
+		Fraction operator*(const Fraction& fr);
+		Fraction operator/(const Fraction& fr);
+		Fraction operator=(const Fraction& fr); 
+
 };
+
 
 
 #endif // INCLUDE_FRACTION_H_
