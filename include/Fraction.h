d0deaad937851dfe8f445fc623568176edbee8fe
Irina <pasmanikirina29@gmail.com>
2020-12-01 21:05:52 +0300

    functions upd 8 : partly fixed code

--- include/Fraction.h
+++ include/Fraction.h
@@ -12,19 +12,19 @@ class Fraction {
     int denominator;
 
  public:
-	explicit Fraction(int a = 0, int b = 1);
-	Fraction(const Fraction&);
+    explicit Fraction(int a = 0, int b = 1);
+    Fraction(const Fraction&);
 
-	void normalize();	// закрытый метод сокращения дроби
-	std::string getValue();	// метод, возвращающий изображение дроби
-	int getNumerator();		// вернуть числитель
-	int getDenominator();	// вернуть знаменатель
+    void normalize();	// закрытый метод сокращения дроби
+    std::string getValue();	// метод, возвращающий изображение дроби
+    int getNumerator();		// вернуть числитель
+    int getDenominator();	// вернуть знаменатель
 
-	Fraction operator+(const Fraction&);
-	Fraction operator-(const Fraction&);
-	Fraction operator*(const Fraction&);
-	Fraction operator/(const Fraction&);
-	Fraction& operator=(const Fraction&);
+    Fraction operator+(const Fraction&);
+    Fraction operator-(const Fraction&);
+    Fraction operator*(const Fraction&);
+    Fraction operator/(const Fraction&);
+    Fraction& operator=(const Fraction&);
 };
 
 #endif // FRACTION_H
