0644ca8b05982899f21f7f64eed10164d2b1a1a2
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-12-01 12:35:28 +0300

    Update Fraction.cpp

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -1,66 +1,68 @@
 // Copyright 2020 Konina Tatiana
-
 #include "Fraction.h"
 #include <string>
 
 int nod(int x, int y) {
-	while (x != y and x != 0 and y != 0) {
-		if (x > y)
-			x = x - y;
-		else
-			y = y - x;
-	}
-	return y;
+  while (x != y && x != 0 && y != 0) {
+    if (x > y)
+		x = x - y;
+    else
+		y = y - x;
+  }
+  return y;
 }
 
 Fraction Fraction::normalize() {
-	int nod_res = nod(this->numerator, this->denominator);
-	this->numerator /= nod_res;
-	this->denominator /= nod_res;
-	return *this;
+  int nod_res = nod(this->numerator, this->denominator);
+  this->numerator /= nod_res;
+  this->denominator /= nod_res;
+  return *this;
 }
 
 int Fraction::getDenominator() const {
-	return this->denominator;
+  return this->denominator;
 }
 
 
 int Fraction::getNumerator() const {
-	return this->numerator;
+  return this->numerator;
 }
 
 std::string Fraction::getValue() {
-	Fraction obj = normalize();
-	std::string res = std::to_string(obj.numerator) + "\\" + std::to_string(obj.denominator);
-	return res;
+  Fraction obj = normalize();
+  std::string res = std::to_string(obj.numerator) +
+	  "\\" + std::to_string(obj.denominator);
+  return res;
 }
 
-Fraction Fraction :: operator+(const Fraction&obj) {
-	int num = this->numerator * obj.denominator + this->denominator * obj.numerator;
-	int denom = this->denominator * obj.denominator;
-	return Fraction(num, denom).normalize();
+Fraction Fraction::operator+(const Fraction& obj) {
+  int num = this->numerator * obj.denominator +
+	  this->denominator * obj.numerator;
+  int denom = this->denominator * obj.denominator;
+  return Fraction(num, denom).normalize();
 }
 
-Fraction Fraction :: operator-(const Fraction& obj) {
-	int num = this->numerator * obj.denominator - this->denominator * obj.numerator;
-	int denom = this->denominator * obj.denominator;
-	return Fraction(num, denom).normalize();
+Fraction Fraction::operator-(const Fraction& obj) {
+  int num = this->numerator * obj.denominator -
+	  this->denominator * obj.numerator;
+  int denom = this->denominator * obj.denominator;
+  return Fraction(num, denom).normalize();
 }
 
-Fraction Fraction :: operator*(const Fraction& obj) {
-	int num = this->numerator * obj.numerator;
-	int denom = this->denominator * obj.denominator;
-	return Fraction(num, denom).normalize();
+Fraction Fraction::operator*(const Fraction& obj) {
+  int num = this->numerator * obj.numerator;
+  int denom = this->denominator * obj.denominator;
+  return Fraction(num, denom).normalize();
 }
 
-Fraction Fraction :: operator/(const Fraction& obj) {
-	int num = this->numerator * obj.denominator;
-	int denom = this->denominator * obj.numerator;
-	return Fraction(num, denom).normalize();
+Fraction Fraction::operator/(const Fraction& obj) {
+  int num = this->numerator * obj.denominator;
+  int denom = this->denominator * obj.numerator;
+  return Fraction(num, denom).normalize();
 }
 
-Fraction Fraction :: operator=(const Fraction& obj) {
-	this->numerator = obj.numerator;
-	this->denominator =  obj.denominator;
-	return *this;
+Fraction Fraction::operator=(const Fraction& obj) {
+  this->numerator = obj.numerator;
+  this->denominator =  obj.denominator;
+  return *this;
 }
