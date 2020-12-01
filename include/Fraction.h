2e59d8ebff1d3b406fdef35d2a1a81d8fe2b175b
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2020-12-01 13:58:36 +0300

    All's done

---
+++ include/Fraction.h
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_FRACTION_H_
+#define INCLUDE_FRACTION_H_
+#include <string>
+
+class Fraction {
+private:
+    int numerator, denominator;
+    void normalize();
+public:
+    explicit Fraction(int numerator = 0, int denominator = 1) {
+        this->numerator = numerator;
+        this->denominator = denominator;
+    }
+    Fraction(const Fraction& new_fraction) {
+        this->denominator = new_fraction.denominator;
+        this->numerator = new_fraction.numerator;
+    }
+    std::string getValue();
+    int getNumerator();
+    int getDenominator();
+    Fraction operator+(const Fraction& fr);
+    Fraction operator-(const Fraction& fr);
+    Fraction operator*(const Fraction& fr);
+    Fraction operator/(const Fraction& fr);
+    Fraction& operator=(const Fraction& fr);
+};
+
+#endif
