17d7725675c2358a770776ec56edb610178fd108
MILA <milalupehina@gmail.com>
2020-12-01 20:15:56 +0300

    first try

---
+++ src/Fraction.cpp
@@ -0,0 +1,77 @@
+#include "Fraction.h"
+//+
+int NOD (int down, int up)
+{
+    while (up != down && up != 0 && down != 0) {
+        if (up > down)
+        {
+            up = up - down;
+        }
+        else
+        {
+            down = down - up;
+        }
+    }
+    return down;
+}
+//+
+void Fraction::normalize()
+{
+    int common = NOD(abs(this->denominator),abs(this->numerator));
+    this->denominator /= common;
+    this->numerator /= common;
+}
+//+
+std::string Fraction::getValue()
+{
+    normalize();
+    if (this->denominator == 1)
+        return std::to_string(this->numerator);
+    else
+        return (std::to_string(this->numerator) + "/" + std::to_string(this->denominator));
+}
+//+
+int Fraction::getNumerator() const
+{
+    return numerator;
+}
+//+
+int Fraction::getDenominator() const
+{
+    return denominator;
+}
+
+Fraction Fraction::operator+(Fraction &f)
+{
+    int nnum = this->numerator * f.denominator + this->denominator * f.numerator;
+    int nden = this->denominator * f.numerator;
+    return Fraction(nnum,nden);
+}
+
+Fraction Fraction::operator-(Fraction &f)
+{
+    int nnum = this->numerator * f.denominator - this->denominator * f.numerator;
+    int nden = this->denominator * f.numerator;
+    return Fraction(nnum,nden);
+}
+
+Fraction Fraction::operator*(Fraction &f)
+{
+    int nnum = this->numerator * f.numerator;
+    int nden = this->denominator * f.denominator;
+    return Fraction(nnum,nden);
+}
+
+Fraction Fraction::operator/(Fraction &f)
+{
+    int nnum = this->numerator * f.denominator;
+    int nden = this->denominator * f.numerator;
+    return Fraction(nnum,nden);
+}
+
+Fraction &Fraction::operator=(const Fraction &f)
+{
+    this->numerator = f.numerator;
+    this->denominator = f.denominator;
+    return *this;
+}
---
+++ src/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include "Fraction.h"
+
+int main()
+{
+    //test 1
+    /*
+    Fraction a;
+    a.getNumerator();   //0
+    a.getDenominator(); //1
+    */
+
+    //test 2
+    /*
+    Fraction a(1, 2);
+    a.getNumerator();   //1
+    a.getDenominator(); //2
+    */
+
+    //test 3
+    /*
+    Fraction a(1, 2);
+    Fraction b(a);
+    b.getNumerator(); //1
+    b.getDenominator(); //2
+    */
+
+    //test 4
+    /*
+    Fraction a(1, 2);
+    Fraction b(a);
+    Fraction c;
+    c = a + b;
+    c.getNumerator();//1
+    c.getDenominator();//1
+    */
+
+    //test 5
+    /*
+    Fraction a(1, 2);
+    Fraction b(-1, 2);
+    Fraction c;
+    c = a + b;
+    c.getNumerator(); //0
+    c.getDenominator();//1
+    */
+
+    //test 6
+    /*
+    Fraction a(1, 2);
+    Fraction b(1, 2);
+    Fraction c;
+    c = a - b;
+    c.getNumerator()); //0
+    c.getDenominator());//1
+    */
+
+    //test 7
+    /*
+    Fraction a(1, 3);
+    Fraction b(2, 3);
+    Fraction c;
+    c = a + b;
+    c.getNumerator();//1
+    c.getDenominator();//1
+    */
+
+    //test 8
+    /*
+    Fraction a(1, 2);
+    Fraction b(1, 2);
+    Fraction c;
+    c = a * b;
+    c.getNumerator();//1
+    c.getDenominator();//4
+     */
+
+    //test 9
+    /*
+    Fraction a(1, 5);
+    Fraction b(1, 2);
+    Fraction c;
+    c = a / b;
+    c.getNumerator();//2
+    c.getDenominator();//5
+    */
+
+/*
+    Fraction a(1, 2); // 1/2
+    Fraction b(a);
+    Fraction c;
+    //std::cout << c.getValue(); // 0
+    //c = a + b;
+    //std::cout << c.getValue(); // 1
+    //...
+*/
+}
+
+
