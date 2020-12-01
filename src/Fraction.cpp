e76cb3b98f7d5147c186224bd95dd24a340b088b
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2020-12-01 17:10:15 +0300

    fix den

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -20,12 +20,13 @@ void Fraction::normalize() {
 Fraction::Fraction(int num, int den) {
   if (den == 0)
     throw "Error: Division by 0";
-  if (den != 0) {
+  else if (den != 0 && num != 0) {
     numerator = num;
     denominator = den;
-    if (num != 0)
-      normalize();
+    normalize();
   }
+  else if (num == 0)
+    denominator = 1;
 }
 
 Fraction::Fraction(const Fraction& f) {
