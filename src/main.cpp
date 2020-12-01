b4e8b520827650cfe6616d7970117b0a9c7b4b2b
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2020-12-01 13:58:21 +0300

    All's done

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include "Fraction.h"
+#include <iostream>
+#include <string>
+
+int main() {
+    Fraction test1(4, 7);
+    Fraction& test2(test1);
+    Fraction test3;
+    std::cout << test1.getValue() << std::endl;
+    std::cout << test2.getValue() << std::endl;
+    std::cout << test3.getValue() << std::endl;
+    test3 = test1 + test2;
+    std::cout << test3.getValue() << std::endl;
+    test3 = test1 * test2;
+    std::cout << test3.getValue() << std::endl;
+    test3 = test1 / test2;
+    std::cout << test3.getValue() << std::endl;
+    return 0;
+}
