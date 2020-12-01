b4ec8f5ee36aa76f00757c347c61fa532848c162
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-12-01 21:46:09 +0300

    Small fixes and overload of operators in Fraction.h

--- include/Fraction.h
+++ include/Fraction.h
@@ -5,15 +5,24 @@
 class Fraction {
  private:
   int numerator = 0, denominator = 1;
-  void normalize(); // закрытый метод нормировки(сокращения дроби 2 / 4 -> 1 / 2).
+  void normalize(); // закрытый метод нормировки
+  //(сокращения дроби 2 / 4 -> 1 / 2).
+  int GCD(int one, int two);
+  int LCM(int one, int two);
 
  public:
-  Fraction(int, int);
-  Fraction(Fraction& from) : numerator{ from.numerator },
+  Fraction(int numerator = 0, int denominator = 1);
+  Fraction(const Fraction& from) : numerator{ from.numerator },
   denominator{ from.denominator } {};
-  std::string getValue(); // -метод, возвращающий std::string, содержащую изображение дроби, например "-5/6".Дробь с единичным знаменателем выводится как целое число.
+  std::string getValue(); // -метод, возвращающий std::string, содержащую 
+  //изображение дроби, например "-5/6".Дробь с единичным знаменателем выводится 
+  //как целое число.
   int getNumerator();
   int getDenominator();
-  int GCD(int one, int two);
+  Fraction operator+(const Fraction& second);
+  Fraction operator-(const Fraction& second);
+  Fraction operator*(const Fraction& second);
+  Fraction operator/(const Fraction& second);
+  Fraction& operator=(const Fraction& second);
 };
 #endif  // INCLUDE_FRACTION_H_
