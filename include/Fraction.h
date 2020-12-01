2471f235e3b0b42ff97dc87bce79fb648f32f3db
Michail <mdmurzinov@edu.hse.ru>
2020-12-01 13:10:37 +0300

    Murzinov Michail b18 Ex7 Done

---
+++ include/Fraction.h
@@ -0,0 +1,25 @@
+// Copyright 2020 GHA Test Team
+#ifndef INCLUDE_FRACTION_H_
+#define INCLUDE_FRACTION_H_
+#include <iostream>
+
+class Fraction {
+ private:
+		int numerator, denominator;
+		void normalize();
+		void check_minus();
+		void check_denominator();
+public:
+	Fraction(int = 0, int = 1);
+	Fraction(Fraction&);
+	Fraction& operator=(const Fraction&);
+	Fraction operator+(const Fraction&);
+	Fraction operator-(const Fraction&);
+	Fraction operator*(const Fraction&);
+	Fraction operator/(const Fraction&);
+	std::string getValue() const;
+	int getNumerator() const;
+	int getDenominator() const;
+};
+
+#endif // !INCLUDE_FRACTION_H_
---
+++ src/Fraction.cpp
@@ -0,0 +1,122 @@
+// Copyright 2020 GHA Test Team
+#include "Fraction.h"
+
+
+void Fraction::check_minus() {
+	if (denominator < 0 && numerator < 0 || denominator < 0) {
+		numerator *= -1;
+		denominator *= -1;
+	}
+}
+
+void Fraction::check_denominator() {
+	try {
+		if (denominator == 0)
+			throw -1;
+	}
+	catch (int) {
+		std::cerr << "the denominator is zero" << std::endl;
+		exit(-1);
+	}
+}
+
+Fraction::Fraction(int numerator, int denominator) {
+	this->numerator = numerator;
+	this->denominator = denominator;
+	check_denominator();
+	check_minus();
+	normalize();
+}
+
+Fraction::Fraction(Fraction& f) : numerator(f.numerator), denominator(f.denominator) {
+	check_minus();
+	check_denominator();
+	normalize();
+}
+
+void Fraction::normalize() {
+	bool sign = false;
+	if (numerator < 0) {
+		sign = true;
+		numerator *= -1;
+	}
+	for (int i = 1; i <= numerator && i <= denominator; i++) {
+		if (numerator % i == 0 && denominator % i == 0) {
+			numerator /= i;
+			denominator /= i;
+		}
+	}
+	if (sign)
+		numerator *= -1;
+}
+
+std::string Fraction::getValue() const {
+	std::string result = "";
+	char num1[100] = { '\0' };
+	char num2[100] = { '\0' };
+	_itoa_s(numerator, num1, 10);
+	_itoa_s(denominator, num2, 10);
+	result += num1;
+	result += '/';
+	result += num2;
+	return result;
+}
+
+int Fraction::getNumerator() const {
+	return numerator;
+}
+
+int Fraction::getDenominator() const {
+	return denominator;
+}
+
+Fraction& Fraction::operator=(const Fraction& f) {
+	numerator = f.numerator;
+	denominator = f.denominator;
+	return *this;
+}
+
+Fraction Fraction::operator+(const Fraction& f) {
+	Fraction temp;
+	int num1(numerator), num2(f.numerator), denum1(denominator), denum2(f.denominator);
+	if (denum1 != denum2) {
+		num1 *= denum2;
+		num2 *= denum1;
+		denum2 = denum1 *= denum2;
+	}
+	temp.numerator = num1 + num2;
+	temp.denominator = denum1;
+	temp.normalize();
+	return temp;
+}
+
+Fraction Fraction::operator-(const Fraction& f) {
+	Fraction temp;
+	int num1(numerator), num2(f.numerator), denum1(denominator), denum2(f.denominator);
+	if (denum1 != denum2) {
+		num1 *= denum2;
+		num2 *= denum1;
+		denum2 = denum1 *= denum2;
+	}
+	temp.numerator = num1 - num2;
+	temp.denominator = denum1;
+	temp.normalize();
+	return temp;
+}
+
+Fraction Fraction::operator*(const Fraction& f) {
+	Fraction temp;
+	temp.numerator = numerator * f.numerator;
+	temp.denominator = denominator * f.denominator;
+	temp.normalize();
+	return temp;
+}
+
+Fraction Fraction::operator/(const Fraction& f) {
+	Fraction temp;
+	temp.numerator = numerator * f.denominator;
+	temp.denominator = denominator * f.numerator;
+	temp.check_denominator();
+	temp.normalize();
+	return temp;
+}
