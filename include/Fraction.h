183dd189cd2e874efef728cab053bc3edc4d8c40
MILA <milalupehina@gmail.com>
2020-12-01 20:56:11 +0300

    styled Fraction.h

--- include/Fraction.h
+++ include/Fraction.h
@@ -6,57 +6,59 @@
 
 class Fraction {
 private:
+    
     int numerator, denominator;
 
-    //закрытый метод нормировки (сокращения дроби 2/4 -> 1/2).
+    // закрытый метод нормировки (сокращения дроби 2/4 -> 1/2).
     void normalize();
 
 public:
-    //конструктор с двумя параметрами,
-    //со значениями по-умолчанию (0,1).
+
+    // конструктор с двумя параметрами,
+    // со значениями по-умолчанию (0,1).
     explicit Fraction(int n = 0, int d = 1) {
         this->numerator = n;
         this->denominator = d;
-    };
+    }
 
-    //конструктор копирования.
+    // конструктор копирования.
     Fraction(const Fraction &f) {
         this->numerator = f.numerator;
         this->denominator = f.denominator;
-    };
+    }
 
-    //метод, возвращающий строку типа std::string,
-    //содержащую изображение дроби,
-    //например "1/2" или "-5/6".
-    //Дробь с единичным знаменателем выводится как целое число.
+    // метод, возвращающий строку типа std::string,
+    // содержащую изображение дроби,
+    // например "1/2" или "-5/6".
+    // Дробь с единичным знаменателем выводится как целое число.
     std::string getValue();
 
-    //вернуть числитель.
+    // вернуть числитель.
     int getNumerator() const;
 
-    //вернуть знаменатель.
+    // вернуть знаменатель.
     int getDenominator() const;
 
-    //Реализовать в классе Fraction перегрузку операций:
-    //operator+ - сложение
+    // Реализовать в классе Fraction перегрузку операций:
+    // operator+ - сложение
     Fraction operator+(Fraction &f);
 
-    //operator- - вычитание
+    // operator- - вычитание
     Fraction operator-(Fraction &f);
 
-    //operator* - умножение
+    // operator* - умножение
     Fraction operator*(Fraction &f);
 
-    //operator/ - деление
+    // operator/ - деление
     Fraction operator/(Fraction &f);
 
-    //operator= - присваивание
+    // operator= - присваивание
     Fraction &operator=(const Fraction &f);
 
-    //В случае появления нуля в знаменателе,
-    //необходимо выбрасывать исключение с помощью оператора throw.
+    //В  случае появления нуля в знаменателе,
+    // необходимо выбрасывать исключение с помощью оператора throw.
 };
 
 int NOD(int, int);
 
-#endif //EX07_FRACTION_H
+#endif  // EX07_FRACTION_H
