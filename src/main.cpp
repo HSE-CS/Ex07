2c3f3cd850aaf482d8c82ee6156d6ed7b249eb13
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-12-01 21:48:50 +0300

    added example program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 // Copyright 2020 Dev-will-work
 #include "Fraction.h"
+#include <iostream>
 
 int main() {
-
+  Fraction fract;
+  Fraction another(4, 45);
+  Fraction test(2, 3);
+  Fraction bad(5, 0);
+  std::cout << fract.getDenominator() << std::endl;
+  std::cout << fract.getNumerator() << std::endl;
+  std::cout << fract.getValue() << std::endl;
+  std::cout << (fract + another).getValue() << std::endl;
+  std::cout << (fract - another).getValue() << std::endl;
+  std::cout << (fract * another).getValue() << std::endl;
+  std::cout << (fract / another).getValue() << std::endl;
+  std::cout << (fract = another).getValue() << std::endl;
+  std::cout << (fract = another = test).getValue() << std::endl;
+  std::cout << (fract + bad).getValue() << std::endl;
+  std::cout << (fract - bad).getValue() << std::endl;
+  std::cout << (fract * bad).getValue() << std::endl;
+  std::cout << (fract / bad).getValue() << std::endl;
+  std::cout << (fract = bad).getValue() << std::endl;
 }
