c722ded1b3bd253cb28704a28018d2cad3d51a28
sharg <jenya.sharunov@gmail.com>
2020-12-01 15:27:33 +0300

    fix Style

--- include/Fraction.h
+++ include/Fraction.h
@@ -9,43 +9,43 @@
 #include <string>
 
 class Fraction {
-    private:
-     int numerator = 0, denominator = 0;
+ private:
+    int numerator = 0, denominator = 0;
 
-    public:
-     explicit Fraction(int valueNumerator = 0, int valueDenominator = 1) {
-         if (valueDenominator == 0) {
-             throw "error denominator is zero";
-         } else {
-             numerator = valueNumerator;
-             denominator = valueDenominator;
-         }
-         normalize();
-     }
+ public:
+    explicit Fraction(int valueNumerator = 0, int valueDenominator = 1) {
+        if (valueDenominator == 0) {
+            throw "error denominator is zero";
+        } else {
+            numerator = valueNumerator;
+            denominator = valueDenominator;
+        }
+        normalize();
+    }
 
-     Fraction(const Fraction &valueFraction) {
-         numerator = valueFraction.numerator,
-                 denominator = valueFraction.denominator;
-     }
+    Fraction(const Fraction &valueFraction) {
+        numerator = valueFraction.numerator,
+                denominator = valueFraction.denominator;
+    }
 
 
-     int getNumerator() const;
+    int getNumerator() const;
 
-     int getDenominator() const;
+    int getDenominator() const;
 
-     Fraction operator+(const Fraction &valueFraction) const;
+    Fraction operator+(const Fraction &valueFraction) const;
 
-     Fraction operator-(const Fraction &valueFraction) const;
+    Fraction operator-(const Fraction &valueFraction) const;
 
-     Fraction operator*(const Fraction &valueFraction) const;
+    Fraction operator*(const Fraction &valueFraction) const;
 
-     Fraction operator/(const Fraction &valueFraction) const;
+    Fraction operator/(const Fraction &valueFraction) const;
 
-     Fraction &operator=(const Fraction &valueFraction);
+    Fraction &operator=(const Fraction &valueFraction);
 
-     std::string getValue();
+    std::string getValue();
 
-     void normalize();
+    void normalize();
 };
 
 #endif  // INCLUDE_FRACTION_H_
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 
 int main() {
     Fraction firstFr;
-    std::cout << firstFr.getNumerator() << " " << firstFr.getDenominator() << std::endl;
+    std::cout << firstFr.getNumerator() << firstFr.getDenominator() << std::endl;
     std::cout << firstFr.getValue() << std::endl;
     Fraction secondFr(2, 4);
     Fraction threeFr(firstFr);
