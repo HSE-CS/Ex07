9128507fcbd2cbeba50379107afc37df15ca2e7c
PolinaZharavina <pdzharavina@edu.hse.ru>
2020-12-01 13:05:41 +0300

    test

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Copyright 2020 PollyllyZh
+#include "Fraction.h"
+#include <iostream>
+int main() {
+    Fraction a(1, 2); // 1/2
+    Fraction b(a);
+    Fraction c;
+    std::cout << c.getValue(); // 0
+    c = a + b;
+    std::cout << c.getValue(); // 1
+    return 0;
+}
