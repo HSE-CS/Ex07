2a41e44ef9a5674d6555bd0805ee8810f4d79d29
Irina <pasmanikirina29@gmail.com>
2020-12-01 22:19:02 +0300

    main added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
 // Copyright 2020 Pasmanik Irina
 
 int main() {
+    Fraction a(1, 2);
+    Fraction b(1, 3);
+	
+    Fraction c;
+    std::cout << c.getNumerator() << std::endl;
+    std::cout << c.getDenominator() << std::endl;
+	
+    c = a + b;
+    std::cout << c.getValue() << std::endl;
+	
+    c = a - b;
+    std::cout << c.getValue() << std::endl;
+
+    c = a * b;
+    std::cout << c.getValue() << std::endl;
+
+    c = a / b;
+    std::cout << c.getValue() << std::endl;
+
+    c = a;
+    std::cout << c.getValue() << std::endl;
+
     return 0;
 }
