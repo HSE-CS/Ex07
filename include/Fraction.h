ba70d054c9bb791ca29694a89c41121931311396
Pasha Emshanov <63115710+Dev-will-work@users.noreply.github.com>
2020-12-01 18:58:08 +0300

    fixed constructors, swapped private and public parts and added utility function in Fraction.h

--- include/Fraction.h
+++ include/Fraction.h
@@ -3,14 +3,17 @@
 #define INCLUDE_FRACTION_H_
 #include <string>
 class Fraction {
- public:
-  Fraction(); // -конструктор с двумя параметрами, со значениями по - умолчанию(0, 1).
-  Fraction(); // -конструктор копирования.
-  void normalize(); // -закрытый метод нормировки(сокращения дроби 2 / 4 -> 1 / 2).
-  std::string getValue(); // -метод, возвращающий std::string, содержащую изображение дроби, например "-5/6".Дробь с единичным знаменателем выводится как целое число.
-  int getNumerator(); // -вернуть числитель.
-  int getDenominator(); // -вернуть знаменатель.
  private:
-   int numerator = 0, denominator = 1;
+  int numerator = 0, denominator = 1;
+  void normalize(); // закрытый метод нормировки(сокращения дроби 2 / 4 -> 1 / 2).
+
+ public:
+  Fraction(int, int);
+  Fraction(Fraction& from) : numerator{ from.numerator },
+  denominator{ from.denominator } {};
+  std::string getValue(); // -метод, возвращающий std::string, содержащую изображение дроби, например "-5/6".Дробь с единичным знаменателем выводится как целое число.
+  int getNumerator();
+  int getDenominator();
+  int GCD(int one, int two);
 };
 #endif  // INCLUDE_FRACTION_H_
