bf635f98bae8ced4c1e7058e237464cbc1a640d7
sharg <jenya.sharunov@gmail.com>
2020-12-01 15:23:37 +0300

    fix Style

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 
 int main() {
     Fraction firstFr;
-    std::cout << firstFr.getNumerator() << " " << firstFr.getDenominator() << " Value=" << firstFr.getValue()
-              << std::endl;
+    std::cout << firstFr.getNumerator() << " " << firstFr.getDenominator() << std::endl;
+    std::cout << firstFr.getValue() << std::endl;
     Fraction secondFr(2, 4);
     Fraction threeFr(firstFr);
     firstFr = secondFr + threeFr;
