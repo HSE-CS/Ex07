512f618dbeaf92626521411f12dbb95126834453
Michail <mdmurzinov@edu.hse.ru>
2020-12-01 13:29:12 +0300

    hot fix

--- include/Fraction.h
+++ include/Fraction.h
@@ -3,23 +3,24 @@
 #define INCLUDE_FRACTION_H_
 #include <iostream>
 
+
 class Fraction {
  private:
-		int numerator, denominator;
-		void normalize();
-		void check_minus();
-		void check_denominator();
-public:
-	Fraction(int = 0, int = 1);
-	Fraction(Fraction&);
-	Fraction& operator=(const Fraction&);
-	Fraction operator+(const Fraction&);
-	Fraction operator-(const Fraction&);
-	Fraction operator*(const Fraction&);
-	Fraction operator/(const Fraction&);
-	std::string getValue() const;
-	int getNumerator() const;
-	int getDenominator() const;
+   int numerator, denominator;
+   void normalize();
+   void check_minus();
+   void check_denominator();
+ public:
+	 Fraction(int = 0, int = 1);
+	 Fraction(Fraction&);
+	 Fraction& operator=(const Fraction&);
+	 Fraction operator+(const Fraction&);
+	 Fraction operator-(const Fraction&);
+	 Fraction operator*(const Fraction&);
+	 Fraction operator/(const Fraction&);
+	 std::string getValue() const;
+	 int getNumerator() const;
+	 int getDenominator() const;
 };
 
-#endif // !INCLUDE_FRACTION_H_
+#endif  // INCLUDE_FRACTION_H_
