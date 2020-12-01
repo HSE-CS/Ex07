f66ef176b8686360a209be2b8f6c1d1f0e7f975b
TatyanaKonina <63141760+TatyanaKonina@users.noreply.github.com>
2020-12-01 12:34:51 +0300

    fix typo

--- include/Fraction.h
+++ include/Fraction.h
@@ -1,5 +1,4 @@
 // Copyright 2020 Konina Tatiana
-
 #ifndef INCLUDE_FRACTION_H_
 #define INCLUDE_FRACTION_H_
 
@@ -8,27 +7,26 @@
 
 class Fraction {
  public:
-	explicit Fraction(int numerator = 0, int denominator = 1) {
-		this->numerator = numerator;
-		this->denominator = denominator;
-	}
-	Fraction(const Fraction& obj) {
-		this->denominator = obj.denominator;
-		this->numerator = obj.numerator;
-
-	}
-	Fraction normalize();
-	int getDenominator() const;
-	int getNumerator() const;
-	std::string getValue();
-	Fraction operator+(const Fraction& obj);
-	Fraction operator-(const Fraction& obj);
-	Fraction operator*(const Fraction& obj);
-	Fraction operator/(const Fraction& obj);
-	Fraction operator=(const Fraction& obj);
+  explicit Fraction(int numerator = 0, int denominator = 1) {
+    this->numerator = numerator;
+    this->denominator = denominator;
+  }
+  Fraction(const Fraction& obj) {
+    this->denominator = obj.denominator;
+    this->numerator = obj.numerator;
+  }
+  Fraction normalize();
+  int getDenominator() const;
+  int getNumerator() const;
+  std::string getValue();
+  Fraction operator+(const Fraction& obj);
+  Fraction operator-(const Fraction& obj);
+  Fraction operator*(const Fraction& obj);
+  Fraction operator/(const Fraction& obj);
+  Fraction operator=(const Fraction& obj);
  private:
-	int numerator;
-	int denominator;
+  int numerator;
+  int denominator;
 };
 
 #endif  // INCLUDE_FRACTION_H_
