1bb3f553d9bca8437bc28d2cdd41fb444dfb67b9
Ilya-Tatsenko <66740021+Ilya-Tatsenko@users.noreply.github.com>
2020-12-04 00:18:06 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(){
 Fraction a(1, 3); // 1/2
 Fraction b(0,0);
 Fraction c;
-std::cout << a.getValue() << endl; // 0
+std::cout << a.getValue() << std::endl; // 0
 c = a / b;
 std::cout << c.getValue(); // 1
 }
