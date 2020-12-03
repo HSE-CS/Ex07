721f4d68ace4b5dd1dc515d4307cce17d07ff302
Ilya-Tatsenko <66740021+Ilya-Tatsenko@users.noreply.github.com>
2020-12-03 21:12:49 +0300

    Update Fraction.h

--- include/Fraction.h
+++ include/Fraction.h
@@ -4,22 +4,22 @@
 using namespace std;
 class Fraction{
  private:
- int numerator, denominator;
- void normalize();
+  int numerator, denominator;
+  void normalize();
  public:
- Fraction(){
- int numerator = 0; 
- int denominator = 1;
- }
- Fraction(const Fraction& x){
- this->numerator = x.numerator;
- this->denominator = x.denominator;
- }
- Fraction(int a, int b){
- if (b == 0 && a != 0) throw "Error";
- else{
- numerator = a;
-  denominator = b;
+  Fraction(){
+  int numerator = 0; 
+  int denominator = 1;
+  }
+  Fraction(const Fraction& x){
+  this->numerator = x.numerator;
+  this->denominator = x.denominator;
+  }
+  Fraction(int a, int b){
+  if (b == 0 && a != 0) throw "Error";
+  else{
+   numerator = a;
+   denominator = b;
   }
   }
  string getValue();
