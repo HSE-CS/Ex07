c2e9ec96f37866b8b252b93c702c2b3bdcf68efa
Ilya <ilya.tatsenko@yandex.ru>
2020-12-03 18:41:55 +0300

    2 try

--- include/Fraction.h
+++ include/Fraction.h
@@ -2,37 +2,32 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
-class Fraction {
-private:
-	int numerator, denominator;
-	void normalize();
+class Fraction{
+ private:
+ int numerator, denominator;
+ void normalize();
 public:
-	Fraction()
-	{
-		int numerator = 0;
-		int denominator = 1;
-	}
-	Fraction(const Fraction& x)
-	{
-		this->numerator = x.numerator;
-		this->denominator = x.denominator;
-	}
-	Fraction(int a, int b)
-	{
-		if (b == 0 && a != 0) throw "Error";
-		else
-		{
-			numerator = a;
-			denominator = b;
-		}
-	}
-	string getValue();
-	int getNumerator();
-	int  getDenominator();
-	Fraction operator+(Fraction x);
-	Fraction operator-(Fraction x);
-	Fraction operator*(Fraction x);
-	Fraction operator/(Fraction x);
-	Fraction operator=(Fraction x);
- };
+ Fraction(){
+  int numerator = 0; 
+  int denominator = 1;
+ }
+ Fraction(const Fraction& x){
+  this->numerator = x.numerator;
+  this->denominator = x.denominator;
+ }
+ Fraction(int a, int b){
+  if (b == 0 && a != 0) throw "Error";
+  else{
+   numerator = a;
+   denominator = b;
+  }
+  }
+ string getValue();
+ int getNumerator();
+ int  getDenominator();
+ Fraction operator+(Fraction x);
+ Fraction operator-(Fraction x);
+ Fraction operator*(Fraction x);
+ Fraction operator/(Fraction x);
+ Fraction operator=(Fraction x);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 #include"Fraction.h"
 
-int main()
-{
-	Fraction a(1, 3); // 1/2
-	Fraction b(0,0);
-	Fraction c;
-	std::cout << a.getValue() << endl; // 0
-	c = a / b;
-	std::cout << c.getValue(); // 1
+int main(){
+Fraction a(1, 3); // 1/2
+Fraction b(0,0);
+Fraction c;
+std::cout << a.getValue() << endl; // 0
+c = a / b;
+std::cout << c.getValue(); // 1
 }
