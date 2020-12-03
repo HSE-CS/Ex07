de1fe9cd72e830f212c4553323eef164f43a2f81
Ilya-Tatsenko <66740021+Ilya-Tatsenko@users.noreply.github.com>
2020-12-04 00:23:34 +0300

    Update Fraction.cpp

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -19,6 +19,11 @@ int NOD(int n1, int n2) {
   return div;
 }
 
+Fraction::Fraction() {
+ numerator = 0;
+ denominator = 1;
+}
+
 void Fraction::normalize() {
   int nod = NOD(this->numerator, this->denominator);
   if (nod == 0) {
