73473f9c0783db8f522c1f74b14134f8c18583b7
Kristina Egorova <kristinaeo2001@mail.ru>
2020-12-05 09:47:58 +0300

    Edit style

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -1,87 +1,89 @@
-﻿// Copyright 2020 GHA Test Team
+// Copyright 2020 GHA Test Team
+
 #include "Fraction.h"
 
-#include <cstring>
-#include <cmath>
-
-Fraction::Fraction(int n, int d){ 
+Fraction::Fraction(int n, int d) {
     numerator = n;
     denominator = d;
+    if (denominator == 0) {
+        throw "Divide by 0";
+    }
 }
 
-Fraction::Fraction(const Fraction& c){ 
+Fraction::Fraction(const Fraction &c) {
     numerator = c.numerator;
     denominator = c.denominator;
 }
 
 int gcd(int a, int b) {
-  int t;
-  while (b != 0) {
-    t = b;
-    b = a % b;
-    a = t;
-  }
-  return a;
+    int t;
+    while (b != 0) {
+        t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
 }
 
-void Fraction::normalize(){ 
+void Fraction::normalize() {
     int t = gcd(numerator, denominator);
-    numerator = numerator/t;
-    denominator = denominator/t;
+    numerator = numerator / t;
+    denominator = denominator / t;
 }
 
-std::string Fraction::getValue() const{ 
+std::string Fraction::getValue() const {
     std::string n, d, ret;
     n = std::to_string(numerator);
     d = std::to_string(denominator);
-    ret = n+"/"+d;
+    ret = n + "/" + d;
+    if (denominator == 1) ret = n;
     return ret;
 }
 
-int Fraction::getNumerator() const{ 
+int Fraction::getNumerator() const {
     return numerator;
 }
 
-int Fraction::getDenominator() const{ 
+int Fraction::getDenominator() const {
     return denominator;
 }
 
-Fraction& Fraction::operator=(const Fraction& c){ 
+Fraction &Fraction::operator=(const Fraction &c) {
     numerator = c.numerator;
     denominator = c.denominator;
     return *this;
 }
 
-Fraction Fraction::operator*(const Fraction& c){ 
+Fraction Fraction::operator*(const Fraction &c) {
     int n, m;
-    n = numerator*c.numerator;
-    m = denominator*c.denominator;
+    n = numerator * c.numerator;
+    m = denominator * c.denominator;
     Fraction a(n, m);
     return a;
 }
 
-Fraction Fraction::operator/(const Fraction& c){ 
+Fraction Fraction::operator/(const Fraction &c) {
     int n, m;
-    n = numerator*c.denominator;
-    m = denominator*c.numerator;
+    n = numerator * c.denominator;
+    m = denominator * c.numerator;
     Fraction a(n, m);
     return a;
 }
 
-Fraction Fraction::operator+(const Fraction& c){ 
+Fraction Fraction::operator+(const Fraction &c) {
     int t, a, b;
     t = denominator * c.denominator / gcd(denominator, c.denominator);
     a = numerator * (t / denominator);
     b = c.numerator * (t / c.denominator);
-    Fraction x(a+b, t);
+    Fraction x(a + b, t);
     return x;
 }
 
-Fraction Fraction::operator-(const Fraction& c){ 
+Fraction Fraction::operator-(const Fraction &c) {
     int t, a, b;
-    t = denominator*c.denominator/gcd(denominator, c.denominator);
-    a = numerator*(t/denominator);
-    b = c.numerator*(t/c.denominator);
-    Fraction x(a-b, t);
+    t = denominator * c.denominator / gcd(denominator, c.denominator);
+    a = numerator * (t / denominator);
+    b = c.numerator * (t / c.denominator);
+    Fraction x(a - b, t);
     return x;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
-﻿// Copyright 2020 GHA Test Team
-#include <iostream>
+// Copyright 2020 GHA Test Team
+
 #include "Fraction.h"
 
-int main(){
-   Fraction a(1, 2), b(2, 3), c;
-   c = a/b;
-   std::cout << c.getValue();
-   return 0;
+#include <iostream>
+
+int main() {
+	Fraction a(2, 4);
+	std::cout << a.getNumerator() << std::endl;
+	std::cout << a.getDenominator() << std::endl;
+	a.normalize();
+	std::cout << a.getValue() << std::endl;
+	Fraction b(3, 8), c;
+	c = a / b;
+	std::cout << c.getValue() << std::endl;
+	c = a * b;
+	std::cout << c.getValue() << std::endl;
+	c = a + b;
+	std::cout << c.getValue() << std::endl;
+	c = a - b;
+	std::cout << c.getValue() << std::endl;
+	return 0;
 }
