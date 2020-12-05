748057bd31a9728af990b52d09ede572ada0f47b
Kristina Egorova <kristinaeo2001@mail.ru>
2020-12-05 10:09:29 +0300

    Fix style

--- include/Fraction.h
+++ include/Fraction.h
@@ -7,20 +7,20 @@
 #include <cstring>
 
 class Fraction {
-private:
+ private:
 	int numerator, denominator;
-public:
-	explicit Fraction(int n = 0, int d = 1);
-	Fraction(const Fraction&);
-	void normalize();
-	std::string getValue() const;
-	int getNumerator() const;
-	int getDenominator() const;
-	Fraction& operator=(const Fraction &);
-	Fraction operator+(const Fraction &);
-	Fraction operator-(const Fraction &);
-	Fraction operator*(const Fraction &);
-	Fraction operator/(const Fraction &);
+ public:
+    explicit Fraction(int n = 0, int d = 1);
+    Fraction(const Fraction&);
+    void normalize();
+    std::string getValue() const;
+    int getNumerator() const;
+    int getDenominator() const;
+    Fraction& operator=(const Fraction &);
+    Fraction operator+(const Fraction &);
+    Fraction operator-(const Fraction &);
+    Fraction operator*(const Fraction &);
+    Fraction operator/(const Fraction &);
 };
 
 #endif  // INCLUDE_FRACTION_H_
--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,23 @@
 #include <iostream>
 
 int main() {
-	Fraction a(2, 4);
-	std::cout << a.getNumerator() << std::endl;
-	std::cout << a.getDenominator() << std::endl;
-	a.normalize();
-	std::cout << a.getValue() << std::endl;
-	Fraction b(3, 8), c;
-	c = a / b;
-	std::cout << c.getValue() << std::endl;
-	c = a * b;
-	std::cout << c.getValue() << std::endl;
-	c = a + b;
-	std::cout << c.getValue() << std::endl;
-	c = a - b;
-	std::cout << c.getValue() << std::endl;
-	Fraction s(1, 3), t(2, 3), w;
-	w = s - t;
-	std::cout << w.getNumerator() << std::endl;
-	std::cout << w.getDenominator() << std::endl;
-	return 0;
+    Fraction a(2, 4);
+    std::cout << a.getNumerator() << std::endl;
+    std::cout << a.getDenominator() << std::endl;
+    a.normalize();
+    std::cout << a.getValue() << std::endl;
+    Fraction b(3, 8), c;
+    c = a / b;
+    std::cout << c.getValue() << std::endl;
+    c = a * b;
+    std::cout << c.getValue() << std::endl;
+    c = a + b;
+    std::cout << c.getValue() << std::endl;
+    c = a - b;
+    std::cout << c.getValue() << std::endl;
+    Fraction s(1, 3), t(2, 3), w;
+    w = s - t;
+    std::cout << w.getNumerator() << std::endl;
+    std::cout << w.getDenominator() << std::endl;
+    return 0;
 }
