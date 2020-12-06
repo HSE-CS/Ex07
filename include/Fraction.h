4c9dad8148c769d5ceecf8a2e023f4a3076bb361
Vadukk <Vadik-Tumakov.com@yandex.ru>
2020-12-06 13:50:35 +0300

    Tumakov

---
+++ include/Fraction.h
@@ -0,0 +1,19 @@
+#define INCLUDE_FRACTION_H_
+#include <string>
+
+class Fraction {
+private:
+	int numerator, denominator;
+public:
+	Fraction();
+	explicit Fraction(int a, int b);
+	Fraction(const Fraction& a);
+	void normalize();
+	std::string getValue();
+	int getNumerator() const;
+	int getDenominator() const;
+	Fraction operator+(const Fraction& b);
+	Fraction operator-(const Fraction& b);
+	Fraction operator*(const Fraction& b);
+	Fraction operator/(const Fraction& b);
+};
