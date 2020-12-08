1a4ff6f9156c734c4cb1ac336f3cd688c29bea16
kisozinov <71232712+kisozinov@users.noreply.github.com>
2020-12-08 21:19:23 +0300

    Tabs lives matter!

--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,29 @@
 #include "Fraction.h"
 
 int main() {
-	Fraction a(3, 5);
-	Fraction b(a);
-	Fraction c(8, 9);
-	Fraction d(10, 2);
-	std::cout << a.getValue() << " -- First fraction" << std::endl;
-	std::cout << b.getValue() << " -- Copy of first fraction" << std::endl;
-	std::cout << c.getValue() << " -- Second fraction\n" << std::endl;
-	std::cout << d.getValue()
-		<< " -- Third fraction which was '10/2'\n" << std::endl;
+    Fraction a(3, 5);
+    Fraction b(a);
+    Fraction c(8, 9);
+    Fraction d(10, 2);
+    std::cout << a.getValue() << " -- First fraction" << std::endl;
+    std::cout << b.getValue() << " -- Copy of first fraction" << std::endl;
+    std::cout << c.getValue() << " -- Second fraction\n" << std::endl;
+    std::cout << d.getValue()
+        << " -- Third fraction which was '10/2'\n" << std::endl;
 
-	std::cout << (a + c).getValue()
-		<< " -- First fraction + Second fraction" << std::endl;
-	std::cout << (a - c).getValue()
-		<< " -- First fraction - Second fraction" << std::endl;
-	std::cout << (a * c).getValue()
-		<< " -- First fraction * Second fraction" << std::endl;
-	std::cout << (a / c).getValue()
-		<< " -- First fraction / Second fraction" << std::endl;
-	c = a;
-	std::cout << c.getValue()
-		<< " -- Now the third fraction is equal to first \n"
-		<< std::endl;
+    std::cout << (a + c).getValue()
+        << " -- First fraction + Second fraction" << std::endl;
+    std::cout << (a - c).getValue()
+        << " -- First fraction - Second fraction" << std::endl;
+    std::cout << (a * c).getValue()
+        << " -- First fraction * Second fraction" << std::endl;
+    std::cout << (a / c).getValue()
+        << " -- First fraction / Second fraction" << std::endl;
+    c = a;
+    std::cout << c.getValue()
+        << " -- Now the third fraction is equal to first \n"
+        << std::endl;
 
-	return 0;
+    return 0;
 }
 
