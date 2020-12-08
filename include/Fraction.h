b1448524645b03e0cfbb1feed526d89093fc466a
kisozinov <71232712+kisozinov@users.noreply.github.com>
2020-12-08 21:17:42 +0300

    Tabs lives matter

--- include/Fraction.h
+++ include/Fraction.h
@@ -7,18 +7,18 @@
 
 class Fraction {
  private:
-	int numerator, denominator;
-	void normalize();
+  int numerator, denominator;
+  void normalize();
  public:
-	 explicit Fraction(int = 0, int = 1);
-	 Fraction(const Fraction &fraction);
-	 std::string getValue();
-	 int getNumerator();
-	 int getDenominator();
-	 Fraction operator+(const Fraction &fraction);  // check using references
-	 Fraction operator-(const Fraction &fraction);
-	 Fraction operator*(const Fraction &fraction);
-	 Fraction operator/(const Fraction &fraction);
-	 Fraction &operator=(const Fraction &fraction);
+  explicit Fraction(int = 0, int = 1);
+  Fraction(const Fraction &fraction);
+  std::string getValue();
+  int getNumerator();
+  int getDenominator();
+  Fraction operator+(const Fraction &fraction);  // check using references
+  Fraction operator-(const Fraction &fraction);
+  Fraction operator*(const Fraction &fraction);
+  Fraction operator/(const Fraction &fraction);
+  Fraction &operator=(const Fraction &fraction);
 };
 #endif  // INCLUDE_FRACTION_H_
--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -5,69 +5,69 @@
 #include <cmath>
 
 int gcd(int a, int b) {
-	while (a && b) {
-		if (a >= b)
-			a %= b;
-		else
-			b %= a;
-	}
-	return a | b;
+    while (a && b) {
+        if (a >= b)
+            a %= b;
+        else
+            b %= a;
+    }
+    return a | b;
 }
 
 Fraction::Fraction(int num, int denom) {
-	if (!denom)
-		throw "Denominator is 0!";
-	numerator = num;
-	denominator = denom;
-	this->normalize();
+    if (!denom)
+        throw "Denominator is 0!";
+    numerator = num;
+    denominator = denom;
+    this->normalize();
 }
 
 Fraction::Fraction(const Fraction &fraction) :
-	numerator(fraction.numerator), denominator(fraction.denominator) {
+    numerator(fraction.numerator), denominator(fraction.denominator) {
 }
 
 std::string Fraction::getValue() {
-	if (denominator == 1)
-		return std::to_string(numerator);
-	return std::to_string(numerator) + "/" + std::to_string(denominator);
+    if (denominator == 1)
+        return std::to_string(numerator);
+    return std::to_string(numerator) + "/" + std::to_string(denominator);
 }
 
 void Fraction::normalize() {
-	int nod = gcd(abs(numerator), abs(denominator));
-	numerator /= nod;
-	denominator /= nod;
+    int nod = gcd(abs(numerator), abs(denominator));
+    numerator /= nod;
+    denominator /= nod;
 }
 
 int Fraction::getNumerator() {
-	return numerator;
+    return numerator;
 }
 
 int Fraction::getDenominator() {
-	return denominator;
+    return denominator;
 }
 
 Fraction Fraction::operator+(const Fraction &f) {
-	return Fraction(numerator * f.denominator + f.numerator * denominator,
-	 denominator * f.denominator);
+    return Fraction(numerator * f.denominator + f.numerator * denominator,
+     denominator * f.denominator);
 }
 
 Fraction Fraction::operator-(const Fraction &f) {
-	return Fraction(numerator * f.denominator - f.numerator * denominator,
-	 denominator * f.denominator);
+    return Fraction(numerator * f.denominator - f.numerator * denominator,
+     denominator * f.denominator);
 }
 
 Fraction Fraction::operator*(const Fraction &f) {
-	return Fraction(numerator * f.numerator,
-	 denominator * f.denominator);
+    return Fraction(numerator * f.numerator,
+     denominator * f.denominator);
 }
 
 Fraction Fraction::operator/(const Fraction &f) {
-	return Fraction(numerator * f.denominator,
-	 denominator * f.numerator);
+    return Fraction(numerator * f.denominator,
+     denominator * f.numerator);
 }
 
 Fraction &Fraction::operator=(const Fraction &f) {
-	numerator = f.numerator;
-	denominator = f.denominator;
-	return *this;
+    numerator = f.numerator;
+    denominator = f.denominator;
+    return *this;
 }
