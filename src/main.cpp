f955c07e930302a2d9ebe4bb4119a2f7a9654f8a
Right-bugor <71226571+Right-bugor@users.noreply.github.com>
2020-12-13 16:32:57 +0300

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,23 @@
 #include "Fraction.h"
 
 int main() {
-	Fraction fr1(0, 1);
-	std::cout << fr1.getNumerator() << '\n';
-	std::cout << fr1.getDenominator() << '\n';
-	std::cout << fr1.getValue() << '\n';
+    Fraction fr1(0, 1);
+    std::cout << fr1.getNumerator() << '\n';
+    std::cout << fr1.getDenominator() << '\n';
+   	std::cout << fr1.getValue() << '\n';
 
-	Fraction fr2(fr1);
-	std::cout << fr2.getValue() << '\n';
-	
-	Fraction fr3(3, 1);
-	Fraction fr4(9, 2);
-	
-	fr1 = fr3;
-	std::cout << fr1.getValue() << '\n';
-	std::cout << (fr3 + fr4).getValue() << '\n';
-	std::cout << (fr3 - fr4).getValue() << '\n';
-	std::cout << (fr3 * fr4).getValue() << '\n';
-	std::cout << (fr3 / fr4).getValue() << '\n';
+    Fraction fr2(fr1);
+    std::cout << fr2.getValue() << '\n';
 
-	return 0;
+    Fraction fr3(3, 1);
+    Fraction fr4(9, 2);
+
+    fr1 = fr3;
+    std::cout << fr1.getValue() << '\n';
+    std::cout << (fr3 + fr4).getValue() << '\n';
+    std::cout << (fr3 - fr4).getValue() << '\n';
+    std::cout << (fr3 * fr4).getValue() << '\n';
+    std::cout << (fr3 / fr4).getValue() << '\n';
+
+    sreturn 0;
 }
