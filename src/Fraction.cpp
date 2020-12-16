9e6459d1a7021126744f2cc87b25ce175e4625a9
Alexander Grachev <s89519011368@yandex.ru>
2020-12-16 16:26:09 +0300

    FractionCPP first try

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -1,3 +1,102 @@
 // Copyright 2020 Grachev Alexander
 
 #include "../include/Fraction.h"
+
+#include <string>
+
+using namespace std;
+
+Fraction::Fraction() {
+    numerator = 0;
+    denominator = 1;
+}
+
+Fraction::Fraction(int num, int denom) {
+    numerator = nu;
+    denominator = den;
+    if (denominator == 0) throw;
+
+}
+
+Fraction::Fraction(const Fraction &frac) {
+    numerator = frac.numerator;
+    denominator = frac.denominator;
+}
+
+string Fraction::GetValue() {
+    string s;
+    s += to_string(numerator);
+    s += '/';
+    s += to_string(denominator);
+}
+
+int Fraction::getNumerator() { return numerator; }
+
+int Fraction::getDenominator() { return denominator; }
+
+void Fraction::normalize() {
+    int min = 0;
+    int nod = 1;
+
+    if (abs(numerator) < abs(denominator)) {
+        min = abs(numerator);
+    }
+
+    if ((abs(numerator) >= abs(denominator))) {
+        min = abs(denominator);
+    }
+
+    for (int i = 1; i <= min; ++i) {
+        if ((numerator % i == 0) && (denominator % i == 0) && (i > nod)) {
+            nod = i;
+        }
+    }
+    numerator /= nod;
+    denominator /= nod;
+}
+
+Fraction Fraction::operator+(const Fraction &frac) {
+    int num = numerator * frac.denominator + frac.numerator * denominator;
+    int denom = denominator * frac.denominator;
+
+    Fraction res(num, denom);
+    res.normalize();
+    return res;
+}
+
+Fraction Fraction::operator-(const Fraction &frac) {
+    int num = numerator * frac.denominator - frac.numerator * denominator;
+    int denom = denominator * frac.denominator;
+
+    Fraction res(num, denom);
+    res.normalize();
+    return res;
+}
+
+Fraction Fraction::operator*(const Fraction &frac) {
+    int num = numerator * frac.numerator;
+    int denom = denominator * frac.denominator;
+
+    Fraction res(num, denom);
+    res.normalize();
+    return res;
+}
+
+Fraction Fraction::operator/(const Fraction &frac) {
+    int num = numerator * frac.denominator;
+    int denom = denominator * frac.numerator;
+
+    Fraction res(num, denom);
+    res.normalize();
+    return res;
+}
+
+Fraction Fraction::operator=(const Fraction &frac) {
+    int num = frac.denominator;
+    int denom = frac.numerator;
+
+    Fraction res(num, denom);
+    res.normalize();
+    return res;
+}
+
