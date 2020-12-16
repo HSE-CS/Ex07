6ad307a96b4ce84f6ce9ace7ef2244c0a9aa5ac2
Alexander Grachev <s89519011368@yandex.ru>
2020-12-16 15:55:57 +0300

    FractionH

--- include/Fraction.h
+++ include/Fraction.h
@@ -3,6 +3,29 @@
 #ifndef EX07_FRACTION_H
 #define EX07_FRACTION_H
 
+#include <iostream>
+
+using namespace std;
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+    void normalize();
+public:
+    Fraction();
+    Fraction(int, int);
+    Fraction(const Fraction&);
+    string GetValue();
+    int getNumerator();
+    int getDenominator();
+
+    Fraction operator + (const Fraction&);
+    Fraction operator - (const Fraction&);
+    Fraction operator / (const Fraction&);
+    Fraction operator * (const Fraction&);
+    Fraction operator = (const Fraction&);
+};
 
 
 #endif //EX07_FRACTION_H
