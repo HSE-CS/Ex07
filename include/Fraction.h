a740c3223ab96c52167b0a95af61054efb222a03
Vadukk <71219248+Vadukk@users.noreply.github.com>
2020-12-19 21:18:17 +0300

    Update Fraction.h

--- include/Fraction.h
+++ include/Fraction.h
@@ -1,24 +1,28 @@
+#ifndef EX07_INCLUDE_FRACTION_H_
+#define EX07_INCLUDE_FRACTION_H_
 #ifndef INCLUDE_FRACTION_H_
 #define INCLUDE_FRACTION_H_
+
+#include <cmath>
+#include <iostream>
+#include <numeric>
 #include <string>
 class Fraction {
-private:
-	int numerator;
-	int denominator;
-	void normalize();
-
-public:
-	std::string getValue();
-	int getNumerator();
-	int getDenominator();
-	explicit Fraction(int = 0, int = 1);
-	Fraction(const Fraction& fraction);
-	Fraction operator+(const Fraction& fraction);
-	Fraction operator-(const Fraction& fraction);
-	Fraction operator*(const Fraction& fraction);
-	Fraction operator/(const Fraction& fraction);
-	Fraction operator=(const Fraction& fraction);
-
+ private:
+  int numerator, denominator;
+  void normalize();
+ public:
+  explicit Fraction(int num = 0, int den = 1);
+  Fraction(const Fraction &);
+  std::string getValue();
+  int getNumerator();
+  int getDenominator();
+  Fraction operator+(const Fraction &);
+  Fraction operator-(const Fraction &);
+  Fraction operator*(const Fraction &);
+  Fraction operator/(const Fraction &);
+  Fraction operator=(const Fraction &);
 };
-#endif
-#endif
+
+#endif  // EX07_INCLUDE_FRACTION_H_ 
+#endif  // INCLUDE_FRACTION_H_
