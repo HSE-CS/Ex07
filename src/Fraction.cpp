f6863c63f1b4a02e4b9e07351b94f2d438d03bc1
Ramazan <ramazankka220@gmail.com>
2020-12-20 00:02:33 +0500

    try

--- src/Fraction.cpp
+++ src/Fraction.cpp
@@ -19,8 +19,9 @@ int check_nod(int num, int den) {
         diff *= -1;
         div = check_nod(num, diff);
     }
-    else
+    else {
         div = check_nod(den, diff);
+    }
 
     return div;
 }
