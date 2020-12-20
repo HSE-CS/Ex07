c6551342f3365c8bd79354a5ac32d26d8333f646
RomanMoiseevvv <66863028+RomanMoiseevvv@users.noreply.github.com>
2020-12-20 20:09:11 +0300

    pls

---
+++ include/Fraction.h
@@ -0,0 +1,35 @@
+#ifndef INCLUDE_FRACTION_H_
+#define INCLUDE_FRACTION_H_
+
+#include <string>
+
+class Fraction {
+ private:
+    int numer;
+    int denom;
+
+    int getGCD() const;
+    void normalize();
+
+ public:
+    explicit Fraction(int numerator = 0, int denominator = 1) {
+        if (denominator == 0)
+            throw "Denominator is zero";
+        this->numer = numerator;
+        this->denom = denominator;
+        normalize();
+    }
+
+    Fraction(const Fraction& f): numer(f.numer), denom(f.denom) {}
+
+    std::string getValue() const;
+    int getNumerator() const;
+    int getDenominator() const;
+    Fraction operator+(const Fraction&) const;
+    Fraction operator-(const Fraction&) const;
+    Fraction operator*(const Fraction&) const;
+    Fraction operator/(const Fraction&) const;
+    Fraction& operator=(const Fraction&);
+};
+
+#endif
